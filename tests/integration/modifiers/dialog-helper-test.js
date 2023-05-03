e5ca0810ab41d8e72e5fe79c86ab5ea8a6d6e26d
Carlos Alba <soradev4@gmail.com>
2023-05-02 18:02:35 -0700

    Dialog and Modal Helper tests

--- tests/integration/modifiers/dialog-helper-test.js
+++ tests/integration/modifiers/dialog-helper-test.js
@@ -1,15 +1,59 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { set } from '@ember/object';
+import { render, setupOnerror, find, settled } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Modifier | dialog-helper', function (hooks) {
   setupRenderingTest(hooks);
 
-  // Replace this with your real tests.
-  test('it renders', async function (assert) {
-    await render(hbs`<div {{dialog-helper}}></div>`);
+  hooks.beforeEach(function () {
+    this.open = false;
+  });
 
-    assert.ok(true);
+  test('it renders', async function (assert) {
+    await render(hbs`
+    <div>
+      <dialog data-test-dialog {{dialog-helper open=this.open}}></dialog>
+      <p data-test-p>Content outside of dialog</p>
+    </div>
+    `);
+    const dialogElem = find('[data-test-dialog]');
+
+    assert.notOk(dialogElem.open, 'The dialog element is disabled');
+    assert.dom('[data-test-p]').hasText('Content outside of dialog');
+  });
+
+  test('it renders and behaves properly', async function (assert) {
+    await render(hbs`
+      <div>
+        <dialog data-test-dialog {{dialog-helper open=this.open}}>
+          <p>Dialog Contents</p>
+        </dialog>
+      </div>
+    `);
+    const dialogElem = find('[data-test-dialog]');
+    assert.notOk(dialogElem.open, 'The dialog element is disabled');
+    set(this, 'open', true);
+    await settled();
+    assert.ok(dialogElem.open, 'The dialog element is open');
+    set(this, 'open', false);
+    await settled();
+    assert.notOk(dialogElem.open, 'The dialog element is disabled');
+  });
+
+  test('it throws error when something other than a dialog element is used', async function (assert) {
+    assert.expect(1);
+
+    setupOnerror((error) => {
+      assert.throws(
+        () => {
+          throw error;
+        },
+        /Assertion Failed: Element must be an instance of the HTMLDialogElement/,
+        'The expected error is thrown'
+      );
+    });
+    await render(hbs`<div {{dialog-helper open=this.open}}></div>`);
   });
 });
--- tests/integration/modifiers/modal-helper-test.js
+++ tests/integration/modifiers/modal-helper-test.js
@@ -1,15 +1,61 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { set } from '@ember/object';
+import { render, setupOnerror, find, settled } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Modifier | modal-helper', function (hooks) {
   setupRenderingTest(hooks);
 
-  // Replace this with your real tests.
-  test('it renders', async function (assert) {
-    await render(hbs`<div {{modal-helper}}></div>`);
+  hooks.beforeEach(function () {
+    this.open = false;
+  });
 
-    assert.ok(true);
+  test('it renders', async function (assert) {
+    await render(hbs`
+      <div>
+        <dialog data-test-modal {{modal-helper open=this.open}}>
+        </dialog>
+        <p data-test-p>Content outside of modal</p>
+      </div>
+    `);
+
+    const modalElem = find('[data-test-modal]');
+
+    assert.notOk(modalElem.open, 'The modal element is disabled');
+    assert.dom('[data-test-p]').hasText('Content outside of modal');
+  });
+
+  test('it renders and behaves properly', async function (assert) {
+    await render(hbs`
+      <div>
+        <dialog data-test-modal {{modal-helper open=this.open}}>
+          <p>Modal Contents</p>
+        </dialog>
+      </div>
+    `);
+    const modalElem = find('[data-test-modal]');
+    assert.notOk(modalElem.open, 'The modal element is disabled');
+    set(this, 'open', true);
+    await settled();
+    assert.ok(modalElem.open, 'The modal element is open');
+    set(this, 'open', false);
+    await settled();
+    assert.notOk(modalElem.open, 'The modal element is disabled');
+  });
+
+  test('it throws error when something other than a dialog element is used', async function (assert) {
+    assert.expect(1);
+
+    setupOnerror((error) => {
+      assert.throws(
+        () => {
+          throw error;
+        },
+        /Assertion Failed: Element must be an instance of the HTMLDialogElement/,
+        'The expected error is thrown'
+      );
+    });
+    await render(hbs`<div {{modal-helper open=this.open}}></div>`);
   });
 });
